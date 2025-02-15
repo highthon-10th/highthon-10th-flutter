aa5263f292428952c25fef9c5cc11818bf45ac73
parkyuhyeon <pyh1111802@naver.com>
2025-02-16 07:25:21 +0900

    default page 퍼블리싱

---
+++ lib/views/default_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:highthon_10th/providers/bottom_naviagtion_bar_provider.dart';
+import 'package:highthon_10th/views/main/main_screen.dart';
+import 'package:highthon_10th/views/my_page/my_page.dart';
+import 'package:highthon_10th/views/registration/registration_page.dart';
+
+import '../commons/bottom_navigation_bar.dart';
+
+class DefaultPage extends StatelessWidget {
+  const DefaultPage({super.key});
+
+  static const List<Widget> pages = [
+    MainScreen(),
+    RegistrationPage(),
+    MyPage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, child) {
+      return Scaffold(
+        body: pages[ref.watch(currentIndexProvider)],
+        bottomNavigationBar: CustomBottomNavBar(),
+      );
+    });
+  }
+}
