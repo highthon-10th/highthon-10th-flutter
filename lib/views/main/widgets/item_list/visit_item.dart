2da4cfe01d4dd683ce11b6124cc9b5d84b244bef
유지우 <yy7826w@gmail.com>
2025-02-16 07:21:54 +0900

    Gesture 삭제

--- lib/views/main/widgets/item_list/visit_item.dart
+++ lib/views/main/widgets/item_list/visit_item.dart
@@ -17,94 +17,84 @@ class VisitItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return VisitDialogModal(visit: visit);
-          },
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Color(0xFF7A618D).withOpacity(0.05),
-              spreadRadius: 0,
-              blurRadius: 4,
-              offset: Offset(0, 4),
-            )
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 236,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(visit.photoUrl), fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(12),
-                ),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFF7A618D).withOpacity(0.05),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: Offset(0, 4),
+          )
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 236,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(visit.photoUrl), fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(12),
               ),
-              const SizedBox(height: 12),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              visit.name,
-                              style: TextStyle(
-                                color: Color(0xFF3B3B3B),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Wanted',
-                              ),
+            ),
+            const SizedBox(height: 12),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            visit.name,
+                            style: TextStyle(
+                              color: Color(0xFF3B3B3B),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Wanted',
                             ),
-                            const SizedBox(width: 6),
-                            Text(
-                              VisitTagsType.values
-                                  .firstWhere((e) => e.name == visit.placeType)
-                                  .text,
-                              style: TextStyle(
-                                color: VisitTagsType.values
-                                    .firstWhere(
-                                        (e) => e.name == visit.placeType)
-                                    .color,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w700,
-                                fontFamily: 'Wanted',
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          visit.description,
-                          style: TextStyle(
-                            color: Color(0xFF6E6E6E),
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: 'Wanted',
                           ),
+                          const SizedBox(width: 6),
+                          Text(
+                            VisitTagsType.values
+                                .firstWhere((e) => e.name == visit.placeType)
+                                .text,
+                            style: TextStyle(
+                              color: VisitTagsType.values
+                                  .firstWhere(
+                                      (e) => e.name == visit.placeType)
+                                  .color,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Wanted',
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        visit.description,
+                        style: TextStyle(
+                          color: Color(0xFF6E6E6E),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Wanted',
                         ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
